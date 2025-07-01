405e5ea25ebbc686b078bc6c3e278b0e5e6572b7
Ahmad Sayeb <ahmadsayeb@Ahmads-MacBook-Pro.local>
2025-06-30 22:13:10 -0400

    from as id

--- app/scratch/main.go
+++ app/scratch/main.go
@@ -23,15 +23,15 @@ func main() {
 }
 
 func run() error {
+	privateKey, err := crypto.LoadECDSA("zblock/accounts/kennedy.ecdsa")
+	if err != nil {
+		return fmt.Errorf("unable to load private key for node: %w", err)
+	}
+
 	tx := Tx{
 		FromID: "Bill",
 		ToID:   "Aaron",
 		Value:  1000,
-	}
-
-	privateKey, err := crypto.LoadECDSA("zblock/accounts/kennedy.ecdsa")
-	if err != nil {
-		return fmt.Errorf("unable to load private key for node: %w", err)
 	}
 
 	data, err := json.Marshal(tx)
@@ -52,6 +52,52 @@ func run() error {
 	// OVER THE WIRE
 
 	publicKey, err := crypto.SigToPub(v, sig)
+	if err != nil {
+		return fmt.Errorf("unable to recover public key from signature: %w", err)
+	}
+
+	fmt.Println("PUB: ", crypto.PubkeyToAddress(*publicKey).String())
+
+	// =================================================================================
+
+	tx = Tx{
+		FromID: "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
+		ToID:   "Frank",
+		Value:  250,
+	}
+
+	data, err = json.Marshal(tx)
+	if err != nil {
+		return fmt.Errorf("unable to marshal %w", err)
+	}
+
+	v2 := crypto.Keccak256(data)
+
+	sig2, err := crypto.Sign(v2, privateKey)
+	if err != nil {
+		return fmt.Errorf("unable to sign transaction: %w", err)
+	}
+
+	fmt.Println("SIG: ", hexutil.Encode(sig2))
+
+	// =================================================================================
+
+	// imagine you misspell it, this is scary! your public address changes!
+	// but you can have from as the public address so you can validate!!!
+	tx2 := Tx{
+		FromID: "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
+		ToID:   "Franks",
+		Value:  250,
+	}
+
+	data, err = json.Marshal(tx2)
+	if err != nil {
+		return fmt.Errorf("unable to marshal %w", err)
+	}
+
+	v2 = crypto.Keccak256(data)
+
+	publicKey, err = crypto.SigToPub(v2, sig2)
 	if err != nil {
 		return fmt.Errorf("unable to recover public key from signature: %w", err)
 	}
